371ac15cee114e0c96554686e662060376fdc174
Max Charrier <m@lilmaax.fr>
2024-04-07 17:56:47 +0200

    Add missing comments (in french)
    Signed-off-by: Max Charrier <m@lilmaax.fr>

--- float32.ml
+++ float32.ml
@@ -1,17 +1,35 @@
-let rec split_string str =
+(**
+  Divise une chaîne de caractère en groupe de 4 caractères.
+  Nécessaire lors de la conversion en hexadécimal
+
+  @author Max Charrier
+  @param str chaîne de caractère
+  @return liste de chaîne de caractère groupé par 4
+*)
+let rec split_string (str : string) : string list =
   let len = String.length str in
   let rec split_helper acc index =
     if index >= len then
       acc
     else
+      (* Soustrait un groupe de 4 caractère à partir de l'index courant *)
       let group = String.sub str index 4 in
+      (* Appel récursif en ajoutant les groupes à la liste *)
       split_helper (group :: acc) (index + 4)
   in
+  (* Inversion pour obtenir l'ordre cohérent *)
   List.rev (split_helper [] 0)
 ;;
 
-let bin_to_hex (bin : string) : char =
-  match bin with
+(**
+  Convertit une chaîne de 4 bits en hexadécimal.
+
+  @author Max Charrier
+  @param bits représentation binaire sur 4 bits
+  @return charactère hexadécimal
+*)
+let bin_to_hex (bits : string) : char =
+  match bits with
   | "0000" -> '0'
   | "0001" -> '1'
   | "0010" -> '2'
@@ -31,18 +49,45 @@ let bin_to_hex (bin : string) : char =
   | _ -> failwith "Entrée non valide pour une chaîne binaire"
 ;;
 
+(**
+  Détermine le bit de signe du nombre flottant.
+
+  @author Max Charrier
+  @param x nombre flottant
+  @return bit de sign (0 -> positif; 1 -> négatif)
+*)
 let sign_bit (x : float) : int =
   if x < 0. then 1 else 0
 ;;
 
+(**
+  Convertit un nombre entier en nombre binaire.
+
+  @author Max Charrier
+  @param n nombre entier
+  @return liste de bits
+*)
 let int_to_binary (n : int) : int list =
   let rec aux (n : int) (acc : int list) : int list =
-    if n = 0 then acc
-    else aux (n / 2) ((n mod 2) :: acc)
+    if n = 0 then
+      acc
+    else
+      (* On divise n et on ajoute son reste à la liste *)
+      aux (n / 2) ((n mod 2) :: acc)
   in
   aux n []
 ;;
 
+(**
+  Calcul l'exposant d'un nombre flottant.
+
+  Récursive terminale.
+
+  @author Max Charrier
+  @param x nombre flottant
+  @param acc accesseur pour calculer l'exposant
+  @return exposant du nombre flottant
+*)
 let rec exponents_bits_aux (x : float) (acc : int) : int =
   if x >= 2. then
     exponents_bits_aux (x /. 2.) (acc + 1)
@@ -52,6 +97,13 @@ let rec exponents_bits_aux (x : float) (acc : int) : int =
     acc
 ;;
 
+(**
+  Détermine les 8 bits d'exposant du nombre flottant.
+
+  @author Max Charrier
+  @param x nombre flottant
+  @return liste de bits
+*)
 let exponent_bits (x : float) : int list =
   let bias : int = 127 in
   let exponent : int = exponents_bits_aux x 0 in
@@ -59,18 +111,34 @@ let exponent_bits (x : float) : int list =
   int_to_binary (exponent + bias)
 ;;
 
+(**
+  Convertit la partie décimale d'un nombre flottant en nombre binaire.
+
+  @author Max Charrier
+  @param f nombre flottant
+  @return liste de bits
+*)
 let fract_to_binary (f : float) : int list =
   let rec aux (f : float) (acc : int list) : int list =
     if f = 0. then
       acc
     else
-      let f' = f *. 2. in
-      let digit = int_of_float f' in
-      aux (f' -. float_of_int digit) (acc @ [digit])
+      let f' = f *. 2. in (* Multiplication par 2 pour le prochain bit *)
+      let digit = int_of_float f' in (* Partie entière uniquement *)
+      aux (f' -. float_of_int digit) (digit :: acc)
   in
-  aux f []
+  (* Inversion pour obtenir l'ordre cohérent *)
+  List.rev (aux f [])
 ;;
 
+(**
+  Réduit les élements de la liste à une longueur fixe.
+
+  @author Max Charrier
+  @param n longueur voulue
+  @param lst liste à réduire
+  @return liste réduite
+*)
 let take (n : int) (lst : 'a list) : 'a list =
   let rec aux n acc = function
     | _ when n <= 0 -> List.rev acc
@@ -80,6 +148,13 @@ let take (n : int) (lst : 'a list) : 'a list =
   aux n [] lst
 ;;
 
+(**
+  Détermine les 23 bits de mantisse du nombre flottant.
+
+  @author Max Charrier
+  @param x nombre flottant
+  @return liste de bits
+*)
 let significand_bits (x : float) : int list =
   let integer_part : int = int_of_float x in
   let integer_binary : int list = int_to_binary integer_part in
@@ -99,6 +174,13 @@ let significand_bits (x : float) : int list =
     padded_bits
 ;;
 
+(**
+  Fournit la représeantion binaire et hexadécimale d'un nombre flottant au format IEEE 754 simple précision.
+
+  @author Max Charrier
+  @param x nombre flottant
+  @return représentation binaire et hexadécimale
+*)
 let float32 (x : float) : string =
   let msg : string = "IEEE 754: Encode FP32 \"" ^ string_of_float x ^ "\" -> " in
 
@@ -123,19 +205,36 @@ let float32 (x : float) : string =
     msg ^ float_str ^ " (" ^ hex_str ^ ")"
 ;;
 
+(**
+  Décode un nombre codé au format IEEE 754 simple précision en nombre réel.
+
+  Opération inverse de `float32`.
+
+  @author Max Charrier
+  @param bits nombre binaire
+  @return nombre flottant
+*)
 let float32_decoder (bits : string) : float =
+  (* Bit de signe *)
   let sign_bit = if bits.[0] = '1' then -1.0 else 1.0 in
+
+  (* Bits d'exposant *)
   let exponent = ref 0 in
   for i = 1 to 8 do
     exponent := !exponent * 2 + (int_of_char bits.[i] - int_of_char '0')
   done;
-  let fraction = ref 0.0 in
+
+  (* Bits de mantisse *)
+  let fraction = ref 0. in
   for i = 9 to 31 do
-    fraction := !fraction +. float_of_int (int_of_char bits.[i] - int_of_char '0') *. (2.0 ** (-.float_of_int (i - 8)))
+    fraction := !fraction +. float_of_int (int_of_char bits.[i] - int_of_char '0') *. (2. ** (-.float_of_int (i - 8)))
   done;
-  sign_bit *. (2.0 ** (float_of_int (!exponent - 127))) *. (1.0 +. !fraction)
+
+  (* Combinaison selon la formule, ne pas oublier de soustraire le biais *)
+  sign_bit *. (2. ** (float_of_int (!exponent - 127))) *. (1. +. !fraction)
 ;;
 
+(* Command-line Arguments *)
 let () =
   if Array.length Sys.argv > 3 then
     failwith "IEEE 754: One number at a time."
